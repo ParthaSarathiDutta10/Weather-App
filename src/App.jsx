1fe23b97beb728d8bae48506b469e906fde00299
Partha Sarathi Dutta <coder.partha08@gmail.com>
2026-07-20 22:26:00 +0600

    Clean Function work

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-// import { useState } from 'react'
-
-
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [count , setCount] =useState(0);
+
+ useEffect(()=>{
+  const timer=setInterval(() => {
+      setCount((prev) => prev+1);
+    },1000);
+
+    return () => clearInterval(timer);
+ },[]);
 
 
   return (
-    <>
-
-    </>
+    <div className='container' >
+      <div className="counter">
+        <p>My Subscribers On Youtube.</p>
+          <div className="odometer" id='odometer'>
+            {count}
+          </div>
+          <h3 className='title' >
+            Subscribers <br /> Realtime Counter
+          </h3>
+      </div>
+    </div>
   )
 }
 
